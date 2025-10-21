4a3a1651b50520cb3bd91a079adcf988cc15e1a7
edw00rd <nick.heller@gmail.com>
2025-10-21 15:53:13 -0600

    Refactor Salmon Map layout and add Polyline
    Updated the layout and styling of the Salmon Map component, including a Polyline for the Lummi River Section.

--- salmon-map/src/App.js
+++ salmon-map/src/App.js
@@ -1,17 +1,32 @@
-import { useState } from "react";
 import { MapContainer, TileLayer, Polyline, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 function App() {
-  // your map code here
+  const center = [48.8239, -122.5951];
+
   return (
-    <div>
-      <h2>Salmon Map</h2>
-      <MapContainer center={[48.8239, -122.5951]} zoom={11} style={{ height: "70vh", width: "100%" }}>
+    <div style={{ height: "100vh", width: "100%", margin: 0, padding: 0 }}>
+      <h2 style={{ textAlign: "center", margin: "10px 0" }}>Salmon Map</h2>
+      <MapContainer
+        center={center}
+        zoom={11}
+        style={{ height: "85vh", width: "100%" }}
+      >
         <TileLayer
-          attribution='&copy; <a href=\"http://osm.org/copyright\">OpenStreetMap</a> contributors'
+          attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        <Polyline
+          positions={[
+            [48.8239, -122.5951],
+            [48.85, -122.58],
+            [48.87, -122.56],
+          ]}
+          color="#0077B6"
+          weight={5}
+        >
+          <Popup>Lummi River Section</Popup>
+        </Polyline>
       </MapContainer>
     </div>
   );
