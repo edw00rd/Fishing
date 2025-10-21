7228a063aa01300e14a3041b0636e63eccf1d12e
edw00rd <nick.heller@gmail.com>
2025-10-21 15:00:24 -0600

    Implement Salmon Map with react-leaflet
    Added a map component to display a Salmon Map using react-leaflet.

--- salmon-map/src/App.js
+++ salmon-map/src/App.js
@@ -1,3 +1,20 @@
 import { useState } from "react";
 import { MapContainer, TileLayer, Polyline, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+
+function App() {
+  // your map code here
+  return (
+    <div>
+      <h2>Salmon Map</h2>
+      <MapContainer center={[48.8239, -122.5951]} zoom={11} style={{ height: "70vh", width: "100%" }}>
+        <TileLayer
+          attribution='&copy; <a href=\"http://osm.org/copyright\">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
+  );
+}
+
+export default App;
